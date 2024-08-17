60202085b1c0d5b1b111a0297bb8f013742143ad
Ryan Lo <lowc1012@gmail.com>
2024-08-17 14:31:04 +0800

    101.symmetric-tree.go

---
+++ 101.symmetric-tree.go
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=101 lang=golang
+ *
+ * [101] Symmetric Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	return isMirror(root.Left, root.Right)	
+}
+
+func isMirror(t1, t2 *TreeNode) bool {
+	if t1 == nil && t2 == nil {
+		return true
+	}
+
+	if t1 == nil || t2 == nil {
+		return false
+	}
+
+	return t1.Val == t2.Val && isMirror(t1.Right, t2.Left) && isMirror(t1.Left, t2.Right)
+}
+
+// @lc code=end
+
