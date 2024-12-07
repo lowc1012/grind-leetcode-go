bd820dcfe1d681ae85c231bb32eac4534ede28ea
Ryan Lo <lowc1012@gmail.com>
2024-12-07 14:58:24 +0800

    update 200.number-of-islands.go to use iterative DFS

--- 200.number-of-islands.go
+++ 200.number-of-islands.go
@@ -5,6 +5,7 @@
  */
 
 // @lc code=start
+// time complexity: O(M * N)
 func numIslands(grid [][]byte) int {
 	count := 0
 	for i := 0; i < len(grid); i++ {
@@ -18,13 +19,33 @@ func numIslands(grid [][]byte) int {
 	return count
 }
 
+// what if the matrix is too large?
+// we can use a stack to simulate the recursive call to avoid deep recursion
+
+// iterative DFS to mark the connected land cells as visited
 func dfs(grid [][]byte, m, n int) {
-	if m >= 0 && n >= 0 && m < len(grid) && n < len(grid[0]) && grid[m][n] == '1' {
-		grid[m][n] = '$'
-		dfs(grid, m+1, n)
-		dfs(grid, m-1, n)
-		dfs(grid, m, n+1)
-		dfs(grid, m, n-1)
+	// use a stack to simulate the recursive call
+	stack := make([][]int, 0)
+	directions := [][]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}
+
+	// push the current land cell into the stack
+	stack = append(stack, []int{m, n})
+
+	// mark the current cell as visited
+	for len(stack) > 0 {
+		// pop the top element from the stack
+		cell := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		row, col := cell[0], cell[1]
+		if (row >= 0 && row < len(grid)) && (col >= 0 && col < len(grid[0])) && grid[row][col] == '1' {
+			grid[row][col] = 'v' // mark as visited
+
+			// push the neighbors into the stack
+			for _, dir := range directions {
+				stack = append(stack, []int{row + dir[0], col + dir[1]})
+			}
+		}
 	}
 }
 
