0fd0ed19a7548bc35f507ae68f96c87139dc88d4
Ryan Lo <ryan.lo@fazzfinancial.com>
2025-01-04 12:53:06 +0800

    26.remove-duplicates-from-sorted-array.go

--- 26.remove-duplicates-from-sorted-array.go
+++ 26.remove-duplicates-from-sorted-array.go
@@ -6,7 +6,17 @@
 
 // @lc code=start
 func removeDuplicates(nums []int) int {
-    
+	slow, fast := 0, 1
+	for fast < len(nums) {
+		if nums[slow] != nums[fast] {
+			// move slow pointer forward and replace the value
+			slow++
+			nums[slow] = nums[fast]
+		}
+		fast++
+	}
+	return slow + 1
 }
+
 // @lc code=end
 
