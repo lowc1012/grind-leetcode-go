b3c91bfabe3c3f92453efa5833b0815d5209ba90
Ryan Lo <ryan.lo@fazzfinancial.com>
2025-03-23 16:29:27 +0800

    update 169.majority-element.go

--- 169.majority-element.go
+++ 169.majority-element.go
@@ -6,50 +6,29 @@
 
 // @lc code=start
 // (method 1) use quick sort (sort.Ints) => TC: O(nlogn), SC: O(1)
-// (method 2) use quick selection algorithm => TC: O(n), SC: O(1)
-import "math/rand"
-
+// (method 2) use hash table => TC: O(n), SC: O(n)
+// (method 3) Boyer-Moore Voting Algorithm => TC: O(n), SC: O(1)
 func majorityElement(nums []int) int {
-	// use quick selection
-	kthSmall := 1 + len(nums)/2
+	// We can use Boyer-Moore without hash table to save memory space
 
-	return quickSelect(&nums, kthSmall, 0, len(nums)-1)
+	// initialize the result and the frequency of current majority
+    result, majority := nums[0], 0
+
+    // iterate each elements in nums
+    for i := 0; i < len(nums); i++ {
+        // select a new candidate from the current element
+		// when the frequency is 0 
+		if majority == 0 {
+            result = nums[i]
+        }
+		// increase the majority if we encounter the same element
+        if nums[i] == result {
+            majority++
+        } else {
+			// If we encounter a different element, we decrease it
+            majority--
+        }
+    }
+
+    return result
 }
-
-func quickSelect(nums *[]int, kthSmall, start, end int) int {
-	if start == end {
-		return (*nums)[start]
-	}
-
-	pivotIndex := start + rand.Intn(end-start)
-
-	selectedIndex := partition(nums, start, end, pivotIndex)
-	if selectedIndex > kthSmall-1 {
-		return quickSelect(nums, kthSmall, start, selectedIndex-1)
-	} else if selectedIndex < kthSmall-1 {
-		return quickSelect(nums, kthSmall, selectedIndex+1, end)
-	} else {
-		return (*nums)[selectedIndex]
-	}
-}
-
-func partition(nums *[]int, start, end, pivotIndex int) int {
-	pivotVal := (*nums)[pivotIndex]
-
-	(*nums)[pivotIndex], (*nums)[end] = (*nums)[end], (*nums)[pivotIndex]
-
-	nextLeftIndex := start
-	for i := start; i < end; i++ {
-		if (*nums)[i] < pivotVal {
-			(*nums)[i], (*nums)[nextLeftIndex] = (*nums)[nextLeftIndex], (*nums)[i]
-			nextLeftIndex++
-		}
-	}
-
-	(*nums)[nextLeftIndex], (*nums)[end] = (*nums)[end], (*nums)[nextLeftIndex]
-
-	return nextLeftIndex
-}
-
-// @lc code=end
-
