6064556bf84a4710f7587f761f4fc970990bd992
Ryan Lo <ryan.lo@fazzfinancial.com>
2025-04-02 21:49:38 +0800

    7.reverse-integer.go

--- 7.reverse-integer.go
+++ 7.reverse-integer.go
@@ -6,7 +6,28 @@
 
 // @lc code=start
 func reverse(x int) int {
-    
+    maxAllowed := math.MaxInt32
+    isNegative := false
+    if x < 0 {
+        isNegative = true
+        x = -x
+        maxAllowed = math.MaxInt32 + 1
+    }
+
+    result := 0
+    for x > 0 {
+        pop := x % 10
+        x /= 10
+
+        if result > maxAllowed/10 || (result == maxAllowed/10 && pop > maxAllowed%10) {
+            return 0
+        }
+        result = pop + result*10
+    }
+
+    if isNegative {
+        result = -result
+    }
+    return result
 }
 // @lc code=end
-
