bee246a5878a9172cf54d70b1acf35c3333c17df
Ryan Lo <ryan.lo@fazzfinancial.com>
2025-04-19 13:31:11 +0800

    update 300.longest-increasing-subsequence

--- 300.longest-increasing-subsequence.go
+++ 300.longest-increasing-subsequence.go
@@ -8,34 +8,37 @@
 
 func lengthOfLIS(nums []int) int {
 	// Dynamic Programming
-    if len(nums) == 0 {
-        return 0
-    }
+	if len(nums) == 0 {
+		return 0
+	}
 
-    // Init DP state
-    dp := make([]int, len(nums))
-    for i := range dp {
-        dp[i] = 1 // LIS is at least one
-    }
+	// Init DP state
+	dp := make([]int, len(nums))
+	for i := range dp {
+		dp[i] = 1 // LIS is at least one
+	}
 
-    // Recurrence Relation
-    for i := 1; i < len(nums); i++ {
-        for j := 0; j < i; j++ {
-            if nums[i] > nums[j] {
-            dp[i] = max(dp[i], dp[j]+1)
-        }
-        }
-    }
+	// Recurrence Relation
+    // For each element at index i (from 1 to n-1), compare it with all elements before it (from 0 to i-1).
+    // If nums[j] < nums[i] (where j ranges from 0 to i-1), it means the element at index i can be a part of a longer subsequence.
+	for i := 1; i < len(nums); i++ {
+		for j := 0; j < i; j++ {
+            // increasing
+			if nums[i] > nums[j] {
+				dp[i] = max(dp[i], dp[j]+1)
+			}
+		}
+	}
 
-    // Find the final result
-    maxLength := 1
-    for _, v := range dp {
-        if v > maxLength {
-            maxLength = v
-        }
-    }
+	// Find the final result
+	maxLength := 1
+	for _, v := range dp {
+		if v > maxLength {
+			maxLength = v
+		}
+	}
 
-    return maxLength
+	return maxLength
 }
 
 // @lc code=end
