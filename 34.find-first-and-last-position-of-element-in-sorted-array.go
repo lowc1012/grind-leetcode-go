a7e7803067e7cd1723912945bb1707816f9f761a
Ryan Lo <ryan.lo@fazzfinancial.com>
2025-12-18 00:13:33 +0800

    update 34.find-first-and-last-position-of-element-in-sorted-array.go

--- 34.find-first-and-last-position-of-element-in-sorted-array.go
+++ 34.find-first-and-last-position-of-element-in-sorted-array.go
@@ -6,52 +6,28 @@
 
 // @lc code=start
 // binary search for firt position of element
-func findFirstElement(target int, nums []int) int {
-	left, right := 0, len(nums)-1
-
-	for left <= right {
-		mid := left + (right-left)/2
-
-		if nums[mid] < target {
-			left = mid + 1
-		} else if nums[mid] > target {
-			right = mid - 1
-		} else {
-			// assure first element
-			if mid == 0 || nums[mid-1] != target {
-				return mid
-			}
-			right = mid - 1
-		}
-
-	}
-	return -1
-}
-
-// binary search for last position of element
-func findLastElement(target int, nums []int) int {
-	left, right := 0, len(nums)-1
-
-	for left <= right {
-		mid := left + (right-left)/2
-
-		if nums[mid] < target {
-			left = mid + 1
-		} else if nums[mid] > target {
-			right = mid - 1
-		} else {
-			// assure first element
-			if mid == len(nums)-1 || (nums[mid+1]) != target {
-				return mid
-			}
-			left = mid + 1
-		}
-	}
-	return -1
-}
-
 func searchRange(nums []int, target int) []int {
-	return []int{findFirstElement(target, nums), findLastElement(target, nums)}
+	res := []int{-1, -1}
+    left, right := 0, len(nums)-1
+    for left <= right {
+        mid := left + (right - left) / 2
+        if target == nums[mid] {
+            l, r := mid, mid
+            for l > 0 && nums[l-1] == target {
+                l--
+            }
+            for r < len(nums)-1 && nums[r+1] == target {
+                r++
+            }
+            res = []int{l, r}
+            break
+        } else if target > nums[mid] {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    return res
 }
 
 // @lc code=end
