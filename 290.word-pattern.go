d0c8b1b58d3d9f6f1de2fc13a97cfc80b0689d1d
Ryan Lo <ryan.lo@fazzfinancial.com>
2025-12-18 12:11:17 +0800

    290.word-pattern.go

--- 290.word-pattern.go
+++ 290.word-pattern.go
@@ -4,9 +4,41 @@
  * [290] Word Pattern
  */
 
+import "strings"
+
 // @lc code=start
 func wordPattern(pattern string, s string) bool {
-    
+	words := strings.Split(s, " ")
+	if len(pattern) != len(words) {
+		return false
+	}
+
+	charToWord := make(map[byte]string)
+	wordToChar := make(map[string]byte)
+
+	for i := 0; i < len(pattern); i++ {
+		char := pattern[i]
+		word := words[i]
+
+		if mappedWord, exists := charToWord[char]; exists {
+			if mappedWord != word {
+				return false
+			}
+		} else {
+			charToWord[char] = word
+		}
+
+		if mappedChar, exists := wordToChar[word]; exists {
+			if mappedChar != char {
+				return false
+			}
+		} else {
+			wordToChar[word] = char
+		}
+	}
+
+	return true
 }
+
 // @lc code=end
 
